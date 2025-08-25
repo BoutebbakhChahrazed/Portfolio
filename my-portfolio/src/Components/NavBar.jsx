d9cc1f9909a1bf71fa3bd29ac1db2760e0b4100c
chahrazedboutebbakh <chahrazed.boutebbakh@univ-constantine2.dz>
2025-08-25 18:10:39 +0200

    NavBar Updated

--- my-portfolio/src/Components/NavBar.jsx
+++ my-portfolio/src/Components/NavBar.jsx
@@ -1,17 +1,17 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
 
-const pages = ['About me', 'Projects', 'Education', 'Experience', 'Contact me'];
+const pages = ["About me", "Projects", "Education", "Experience", "Contact me"];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -25,18 +25,25 @@ function ResponsiveAppBar() {
   };
 
   const scrollToSection = (sectionId) => {
-    const element = document.getElementById(sectionId.toLowerCase().replace(' ', '-'));
+    const element = document.getElementById(
+      sectionId.toLowerCase().replace(" ", "-")
+    );
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
+      element.scrollIntoView({ behavior: "smooth" });
     }
     handleCloseNavMenu();
   };
 
   return (
-    <AppBar position="static" sx={{ backgroundColor: '#ffffff', boxShadow: 'none' }}>
+    <AppBar
+      position="static"
+      sx={{ backgroundColor: "#ffffff", boxShadow: "none" }}
+    >
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1, color: 'black' }} />
+          <AdbIcon
+            sx={{ display: { xs: "none", md: "flex" }, mr: 1, color: "black" }}
+          />
           <Typography
             variant="h6"
             noWrap
@@ -44,14 +51,14 @@ function ResponsiveAppBar() {
             href="#home"
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: '#000000',
-              textDecoration: 'none',
-              '&:hover': {
-                color: '#555555',
+              letterSpacing: ".3rem",
+              color: "#000000",
+              textDecoration: "none",
+              "&:hover": {
+                color: "#555555",
               },
             }}
           >
@@ -59,7 +66,7 @@ function ResponsiveAppBar() {
           </Typography>
 
           {/* Mobile menu */}
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -74,18 +81,18 @@ function ResponsiveAppBar() {
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: { xs: "block", md: "none" },
               }}
             >
               {pages.map((page) => (
@@ -95,8 +102,10 @@ function ResponsiveAppBar() {
               ))}
             </Menu>
           </Box>
-          
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1, color: 'black' }} />
+
+          <AdbIcon
+            sx={{ display: { xs: "flex", md: "none" }, mr: 1, color: "black" }}
+          />
           <Typography
             variant="h5"
             noWrap
@@ -104,35 +113,41 @@ function ResponsiveAppBar() {
             href="#home"
             sx={{
               mr: 2,
-              display: { xs: 'flex', md: 'none' },
+              display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: 'monospace',
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'black',
-              textDecoration: 'none',
-              '&:hover': {
-                color: '#555555',
+              letterSpacing: ".3rem",
+              color: "black",
+              textDecoration: "none",
+              "&:hover": {
+                color: "#555555",
               },
             }}
           >
             Chahrazed Boutebbakh
           </Typography>
-          
+
           {/* Desktop menu */}
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end' }}>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              justifyContent: "flex-end",
+            }}
+          >
             {pages.map((page) => (
               <Button
                 key={page}
                 onClick={() => scrollToSection(page)}
                 sx={{
                   my: 2,
-                  color: 'black',
-                  display: 'block',
+                  color: "black",
+                  display: "block",
                   mx: 1,
-                  '&:hover': {
-                    color: '#555555',
-                    backgroundColor: 'transparent',
+                  "&:hover": {
+                    color: "#555555",
+                    backgroundColor: "transparent",
                   },
                 }}
               >
